75af0d42b4fd5a3beebec51c025aeabd2cf338a6
TwoHundredFaces <emersonsopares141@gmail.com>
2023-04-25 08:38:14 -0300

    Carrinho de compras

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelectorAll(el);
+
 //Listagem dos modelos
 modeloJson.map((item, index) => {
-    let modeloItem = document.querySelector('.models .modelo-item').cloneNode(true);
+    let modeloItem = c('.models .modelo-item').cloneNode(true);
 
-    
+    modeloItem.querySelector('.modelo-item-name').innerHTML = item.name;
+
+
+    c('.modelo-area').append(modeloItem);
 })
